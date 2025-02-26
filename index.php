1bc7bca28074a5dfca1092c45644dfdacf78c5d5
lemee <lemeematteo27@gmail.com>
2025-02-26 14:08:13 +0100

    index

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
+
+require_once "View/v_header.php";
+require_once "Model/connexionBdd.php";
+
+$database = new Database();
+$db = $database->getBdd();
+
+$action = $_GET["action"] ?? "accueil";
+
+switch($action){
+    case "accueil":
+        require_once "";
+        break;
+    case "seConnecter": 
+        break;
+}
+
 ?>
