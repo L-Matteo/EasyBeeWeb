bba64b6999ba018514b77bbbc35f0b889c3986c8
lemee <lemeematteo27@gmail.com>
2025-05-28 20:22:53 +0200

    delete file "function.js"

--- Public/js/function.js
+++
@@ -1,5 +0,0 @@
-
-function changerMenu(newMenu, ancienMenu) {
-    document.getElementById(ancienMenu).classList.add("cacher") // TODO : corriger pour que ça fonctionne
-    document.getElementById(newMenu).classList.remove("cacher")
-}
