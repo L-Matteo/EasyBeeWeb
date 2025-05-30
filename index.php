99e8d8af09292848451d5799ff41b5f4acfc255a
lemee <lemeematteo27@gmail.com>
2025-05-30 17:09:16 +0200

    feat: add account feature

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
 <?php
 
+session_start();
+
 require_once "View/v_header.php";
 require_once "Model/connexionBdd.php";
-require_once "Controller/c_accueil.php";
 require_once "Controller/c_stagiaire.php";
 require_once "Controller/c_formation.php";
+require_once "Controller/c_inscription.php";
 
 $database = new Database();
 $db = $database->getBdd();
-session_start();
 
 $action = $_GET["action"] ?? "accueil"; 
 
-$ca = new ControllerAccueil($db);
 $cs = new ControllerStagiaire($db);
 $cf = new ControllerFormation($db);
+$ci = new ControllerInscription($db);
 
 switch($action){
     case "accueil":
         $cs->c_connexion();
-        $ca->c_showFormations(); 
+        $cf->c_showFormations(); 
         break;
     case "createAccount":
         $cs->c_createAccount();
         break;
     case "formation":
-        if($_SERVER["REQUEST_METHOD"] === "POST"){
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["BtnFiltre"])){
             $cf->c_filtrerFormation();
         } else {
-            $ca->c_accueilConnecte();
+            $cf->c_showFormations();
         }
         break;
     case "inscription":
-        if($_SERVER["REQUEST_METHOD"] === "POST"){
-            echo "Vous êtes inscri"; // TODO remplacer pour une fonction
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["btnInscription"])){
+            $ci->c_inscription();
         } else {
             $cf->c_selectFormationById();
         }
         break;
+    case "compte":
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["btnDeco"])) {
+            session_destroy();
+            header("Location: index.php?action=accueil");
+        } else {
+            require_once "View/compte.php";
+        }
 }
 
 require_once "View/v_footer.php";
