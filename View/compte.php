82d74f2655321201e85a54ac3147f1fc0e59c0ec
lemee <lemeematteo27@gmail.com>
2025-05-30 17:02:26 +0200

    feat: add account view

---
+++ View/compte.php
@@ -0,0 +1,7 @@
+    <link href="" rel="stylesheet" />
+</head>
+
+<body>
+    <form action="" method="POST">
+        <input type="submit" class="btn btn-danger" value="Déconnexion" name="btnDeco">
+    </form>
